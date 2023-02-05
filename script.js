915be733737333311fd5f7bef92acba44c7f80b6
mbalint322 <matulabalint.sj@gmail.com>
2023-02-05 21:16:03 +0100

    Can play one round with computer

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const Choices = ["rock", "paper", "scissor"];
 
-let ComputerChoice = getComputerChoice();
+console.log(PlayRound(getPlayerChoice(), getComputerChoice()));
+
+function PlayRound(PlayerChoice, ComputerChoice) {
+    console.log(ComputerChoice);
+    console.log(PlayerChoice);
+    if (PlayerChoice == "rock") {
+        if (ComputerChoice == "scissor") {
+            return "You won! Rock beats Scissor";
+        } 
+        if (ComputerChoice == "rock") {
+            return "It's a tie! You both chose Rock";
+        }
+        else return "You lose! Paper beats Rock";
+    }
+    if (PlayerChoice == "paper") {
+        if (ComputerChoice == "rock") {
+            return "You win! Paper beats Rock";
+        } 
+        if (ComputerChoice == "paper") {
+            return "It's a tie! You both chose Paper";
+        }
+        else return "You lose! Scissor beats Paper";
+    }
+    if (PlayerChoice == "scissor") {
+        if (ComputerChoice == "paper") {
+            return "You won! Scissor beats Paper";
+        } 
+        if (ComputerChoice == "scissor") {
+            return "It's a tie! You both chose Scissor";
+        }
+        else return "You lose! Rock beats Scissor";
+    }
+}
+
+function getPlayerChoice() {
+    let PlayerChoice = prompt("Choose your weapon! Rock, Paper or Scissor?").toLowerCase();
+    return PlayerChoice;
+}
 
 function getComputerChoice() {
     let ComputerChoice = Choices[getRandomInt(3)];
@@ -8,4 +45,5 @@ function getComputerChoice() {
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);}
+    return Math.floor(Math.random() * max);
+}
