971612465a5bc19fc7c9cf8a349a95e5a0051cb9
mbalint322 <matulabalint.sj@gmail.com>
2023-05-01 21:29:18 +0200

    Created buttons, added event listeners, can play rounds

--- script.js
+++ script.js
@@ -1,80 +1,61 @@
-const Choices = ["rock", "paper", "scissor"];
+const Choices = ["rock", "paper", "scissor"]; // Array containing possible choices
 
-game();
+const messageField = document.querySelector('#messageField');
 
-function game() {
-    let PlayerWon = 0;
-    let ComputerWon = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        let result = PlayRound(getPlayerChoice(), getComputerChoice());
-        if (result == 1) {
-            PlayerWon += 1;
-        }
-        if (result == undefined) {
-            PlayerWon += 1;
-            ComputerWon += 1;
-        }
-        if (result == 0) {
-            ComputerWon += 1;
-        }
-        console.log("player: " + PlayerWon);
-        console.log("computer: " + ComputerWon);
-        console.log(result);
-    }
-    if (PlayerWon > ComputerWon) {
-        alert("You won with a score of " + PlayerWon + "points versus the computer's " + ComputerWon + " points.");
-    }
-    else {
-        alert("The computer won with a score of " + ComputerWon + " points versus your " + PlayerWon + " points.");
-    }
-}
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener ('click', () => {
+    PlayRound("rock", getComputerChoice());
+});
+
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener ('click', () => {
+    PlayRound("paper", getComputerChoice());
+});
+
+const scissorButton = document.querySelector('#scissor');
+scissorButton.addEventListener ('click', () => {
+    PlayRound("scissor", getComputerChoice());
+});
 
 function PlayRound(PlayerChoice, ComputerChoice) {
     console.log(ComputerChoice);
     console.log(PlayerChoice);
     if (PlayerChoice == "rock") {
         if (ComputerChoice == "scissor") {
-            alert("You won! Rock beats Scissor")
+            messageField.textContent = "You won! Rock beats Scissor";
             return 1;
         } 
         if (ComputerChoice == "rock") {
-            alert("It's a tie! You both chose Rock")
+            messageField.textContent = "It's a tie! You both chose Rock";
             return undefined;
         }
-        else alert("You lose! Paper beats Rock")
+        else messageField.textContent = "You lose! Paper beats Rock";
         return 0;
     }
     if (PlayerChoice == "paper") {
         if (ComputerChoice == "rock") {
-            alert("You win! Paper beats Rock")
+            messageField.textContent = "You win! Paper beats Rock";
             return 1;
         } 
         if (ComputerChoice == "paper") {
-            alert("It's a tie! You both chose Paper")
+            messageField.textContent = "It's a tie! You both chose Paper";
             return undefined;
         }
-        else alert("You lose! Scissor beats Paper")
+        else messageField.textContent = "You lose! Scissor beats Paper";
         return 0;
     }
     if (PlayerChoice == "scissor") {
         if (ComputerChoice == "paper") {
-            alert("You won! Scissor beats Paper")
+            messageField.textContent = "You won! Scissor beats Paper";
             return 1;
         } 
         if (ComputerChoice == "scissor") {
-            alert("It's a tie! You both chose Scissor")
+            messageField.textContent = "It's a tie! You both chose Scissor";
             return undefined;
         }
-        else alert("You lose! Rock beats Scissor")
+        else messageField.textContent = "You lose! Rock beats Scissor";
         return 0;
     }
-    else return "Please enter a valid choice.";
-}
-
-function getPlayerChoice() {
-    let PlayerChoice = prompt("Choose your weapon! Rock, Paper or Scissor?").toLowerCase();
-    return PlayerChoice;
 }
 
 function getComputerChoice() {
