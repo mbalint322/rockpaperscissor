9507b5fc88bba0e662a923d78893963a4f679ffc
mbalint322 <matulabalint.sj@gmail.com>
2023-06-04 15:44:36 +0200

    Uses buttons to play 5 rounds /n Announces winner /n Hides weapons, shows button to play again

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # rockpaperscissor
 Rock Paper Scissor Game in JS
 
+Things to change: 
+- Score refresh shouldn't be tied to clicking button - DONE, it is tied to the Playround function
+- Button should disappear or stop functioning once 5 points are reached. DONE
+- Make a reset button appear when someone wins. DONE
+- Design UI and story
+- Could add more variety in victory / loss message depending on scorecount
+
 9th of May 2023: Now shows win count and announces winner when 5 points are reached.
 
-Things to change: 
-- Score refresh shouldn't be tied to clicking button
-- Button should disappear or stop functioning once 5 points are reached. Should be replaced with a reset button.
-- Design UI and story
-- Could add more variety in victory / loss message depending on scourecount
+4th of June 2023: 
+- Added hidden attribute toggle to the weapons
+- Weapons disappear after winner is announced
+- Added Play again button, it appears when winner is announced
+
--- script.js
+++ script.js
@@ -5,39 +5,80 @@ const messageField = document.querySelector('#messageField');
 let playerScore = 0;
 let computerScore = 0;
 
+const weapons = Array.from(document.querySelectorAll('.weapon'));
+const playAgainButton = document.querySelector('#playAgain');
+    playAgainButton.addEventListener ('click', () => {
+    playAgain();
+    });
+
 const resultField = document.querySelector('#score');
 resultField.textContent = `Player: ${playerScore}  Computer: ${computerScore}`;
 
 const rockButton = document.querySelector('#rock');
 rockButton.addEventListener ('click', () => {
     PlayRound("rock", getComputerChoice());
+    checkWinner();
 });
 
 const paperButton = document.querySelector('#paper');
 paperButton.addEventListener ('click', () => {
     PlayRound("paper", getComputerChoice());
+    checkWinner();
 });
 
 const scissorButton = document.querySelector('#scissor');
 scissorButton.addEventListener ('click', () => {
     PlayRound("scissor", getComputerChoice());
+    checkWinner();
     });
 
+//Function to reset the game for another match
+function playAgain (){
+    playerScore = 0; 
+    computerScore = 0;
+    console.log(playerScore, computerScore);
+    refreshScore();
+    togglePlayAgainButton();
+    toggleWeapons();
+    messageField.textContent = "";
+}
 
+//Function to make the "Play again!" button appear
+function togglePlayAgainButton(){
+    playAgainButton.toggleAttribute("hidden");
+}
+
+//Function to hide all "weapon" buttons
+function toggleWeapons () {
+    weapons.forEach(weapon => weapon.toggleAttribute("hidden"));
+}
+
+//Checks if player or computer has 5 points and changes message field to say it, then hides weapons
+function checkWinner () {
+    if (playerScore >= 5) {
+    resultField.textContent = `Congratulations! You won!`;
+    toggleWeapons();
+    togglePlayAgainButton()
+    return
+    }
+if (computerScore >= 5) {
+    resultField.textContent = `You lost :( Better luck next time!`;
+    toggleWeapons();
+    togglePlayAgainButton()
+    return
+    }
+    
+}
+//refreshes the score in the message field
 function refreshScore () {
 resultField.textContent = `Player: ${playerScore}  Computer: ${computerScore}`; 
+
 }
+
+//Plays one round of Rock Paper Scissors
 function PlayRound(PlayerChoice, ComputerChoice) {
     console.log(ComputerChoice);
     console.log(PlayerChoice);
-    if (playerScore >= 5) {
-        resultField.textContent = `Congratulation! You won!`;
-        return
-    }
-    if (computerScore >= 5) {
-        resultField.textContent = `You lost :( Better luck next time!`;
-        return
-    }
     if (PlayerChoice == "rock") {
         if (ComputerChoice == "scissor") {
             messageField.textContent = "You won! Rock beats Scissor";
@@ -90,7 +131,7 @@ function PlayRound(PlayerChoice, ComputerChoice) {
             return
     }
 }
-
+//Computer randoms a number between 0-2, choosing Rock/Paper/Scissor from an Array
 function getComputerChoice() {
     let ComputerChoice = Choices[getRandomInt(3)];
     return ComputerChoice;
