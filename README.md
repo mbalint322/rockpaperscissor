8f3aed8f98e1f677c1b88dfde6e747ef5a40e1e2
mbalint322 <matulabalint.sj@gmail.com>
2023-06-11 22:02:48 +0200

    Lots of changes, documented in Readme

--- README.md
+++ README.md
@@ -15,3 +15,31 @@ Things to change:
 - Weapons disappear after winner is announced
 - Added Play again button, it appears when winner is announced
 
+Prompts for pixel arts at Microsoft's AI image creator via Bing:
+
+- Prompt for pixel art logo: "Pixel art of a teenage adventurer boy standing on a field. He is wearing a backpack, a red baseball hat, white shirt and a pair of jeans with white sneakers. Before him on the ground are three objects: a rock, a sheet of paper and a pair of scissors."
+- Prompt for rock pixel art: "pixel art of a rock"
+- Prompt for paper pixel art: "pixel art of a blank paper"
+- Prompt for scissor pixel art: "pixel art of a pair of scissors."
+
+11th of June:
+- Created pixel arts for logo and weapons
+- Created pixel art for monsters
+- Designed look for the page, aligned flexboxes
+- Added health bars, removing health functions correctly
+
+Tried to change the monster image to another monster's image after choosing a weapon,
+but could not get it to work. It could append 1 out of 4 images, but not the rest.
+Still have to figure out a way to make this work.
+ - Fixed - Had to declare the monster images to preload them, otherwise they wouldn't appear when appending.
+
+- Random monsters now appear according to their types.
+
+STILL TO DO: 
+- Hover and on click effects
+- Story and animation for the text to appear
+- Message to appear above weapon icons and not below - DONE
+- Apply character style to appended monsters
+- Winner sequence - Player gets their reward for killing 5 monsters
+- Text for monsters appearing and change of messages according to new design
+- Character image to change depending on what weapon you chose - create more pixel art? Not sure if AI can create consistent character
--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 const Choices = ["rock", "paper", "scissor"]; // Array containing possible choices
 
+//Have to declare the monster images to preload them, otherwise they wouldn't appear when appending
+const scissorMonster0 = document.createElement("img");
+    scissorMonster0.src = "./pixelart/scissor_monsters/scissormonster0.jpeg";
+
+    const scissorMonster1 = document.createElement("img");
+    scissorMonster1.src = "./pixelart/scissor_monsters/scissormonster1.jpeg";
+
+    const scissorMonster2 = document.createElement("img");
+    scissorMonster2.src = "./pixelart/scissor_monsters/scissormonster2.jpeg";
+
+    const scissorMonster3 = document.createElement("img");
+    scissorMonster3.src = "./pixelart/scissor_monsters/scissormonster3.jpeg";
+
+    const scissorMonsters = [scissorMonster0, scissorMonster1, scissorMonster2, scissorMonster3];
+
+const rockMonster0 = document.createElement("img");
+    rockMonster0.src = "./pixelart/rock_monsters/rockmonster0.jpeg";
+
+    const rockMonster1 = document.createElement("img");
+    rockMonster1.src = "./pixelart/rock_monsters/rockmonster1.jpeg";
+
+    const rockMonster2 = document.createElement("img");
+    rockMonster2.src = "./pixelart/rock_monsters/rockmonster2.jpeg";
+
+    const rockMonster3 = document.createElement("img");
+    rockMonster3.src = "./pixelart/rock_monsters/rockmonster3.jpeg";
+
+    const rockMonsters = [rockMonster0, rockMonster1, rockMonster2, rockMonster3];
+
+const paperMonster0 = document.createElement("img");
+    paperMonster0.src = "./pixelart/paper_monsters/papermonster0.jpeg";
+
+    const paperMonster1 = document.createElement("img");
+    paperMonster1.src = "./pixelart/paper_monsters/papermonster1.jpeg";
+
+    const paperMonster2 = document.createElement("img");
+    paperMonster2.src = "./pixelart/paper_monsters/papermonster2.jpeg";
+
+    const paperMonster3 = document.createElement("img");
+    paperMonster3.src = "./pixelart/paper_monsters/papermonster3.jpeg";
+
+    const paperMonsters = [paperMonster0, paperMonster1, paperMonster2, paperMonster3];
+
 const messageField = document.querySelector('#messageField');
 
 let playerScore = 0;
 let computerScore = 0;
 
 const weapons = Array.from(document.querySelectorAll('.weapon'));
-const playAgainButton = document.querySelector('#playAgain');
-    playAgainButton.addEventListener ('click', () => {
-    playAgain();
+const gameButton = document.querySelector('#gameButton');
+gameButton.textContent = "Start New Game";
+gameButton.addEventListener ('click', () => {
+    startNewGame();
     });
 
 const resultField = document.querySelector('#score');
@@ -32,20 +76,55 @@ scissorButton.addEventListener ('click', () => {
     checkWinner();
     });
 
+playerHealth = document.querySelector('.player_health');
+monsterHealth = document.querySelector('.monster_health');
+
+characterContainer = document.querySelector('.character_container');
+
+function createScissorMonster () {
+    characterContainer.removeChild(characterContainer.lastElementChild);
+    characterContainer.appendChild(scissorMonsters[getRandomInt(4)]);
+}
+
+function createRockMonster () {
+    characterContainer.removeChild(characterContainer.lastElementChild);
+    characterContainer.appendChild(rockMonsters[getRandomInt(4)]);
+}
+
+function createPaperMonster () {
+    characterContainer.removeChild(characterContainer.lastElementChild);
+    characterContainer.appendChild(paperMonsters[getRandomInt(4)]);
+}
+
+//Function to remove one health from player
+function playerLoseHealth() {
+    playerHealth.removeChild(playerHealth.lastElementChild);
+}
+
+//Function to remove one health from enemy
+function monsterLoseHealth() {
+    monsterHealth.removeChild(monsterHealth.firstElementChild);
+}
+
 //Function to reset the game for another match
-function playAgain (){
+function startNewGame () {
+    if (playerScore == 5 || computerScore == 5) {
+        document.location.reload();
+    }
+    else {
     playerScore = 0; 
     computerScore = 0;
     console.log(playerScore, computerScore);
     refreshScore();
-    togglePlayAgainButton();
+    togglegameButton();
     toggleWeapons();
     messageField.textContent = "";
+    }
 }
 
 //Function to make the "Play again!" button appear
-function togglePlayAgainButton(){
-    playAgainButton.toggleAttribute("hidden");
+function togglegameButton() {
+    gameButton.toggleAttribute("hidden");
 }
 
 //Function to hide all "weapon" buttons
@@ -58,13 +137,13 @@ function checkWinner () {
     if (playerScore >= 5) {
     resultField.textContent = `Congratulations! You won!`;
     toggleWeapons();
-    togglePlayAgainButton()
+    togglegameButton()
     return
     }
 if (computerScore >= 5) {
     resultField.textContent = `You lost :( Better luck next time!`;
     toggleWeapons();
-    togglePlayAgainButton()
+    togglegameButton()
     return
     }
     
@@ -83,16 +162,21 @@ function PlayRound(PlayerChoice, ComputerChoice) {
         if (ComputerChoice == "scissor") {
             messageField.textContent = "You won! Rock beats Scissor";
             playerScore = playerScore + 1;
+            createScissorMonster ();
+            monsterLoseHealth();
             refreshScore();
             return
         } 
         if (ComputerChoice == "rock") {
             messageField.textContent = "It's a tie! You both chose Rock";
             refreshScore();
+            createRockMonster ();
             return
         }
         else messageField.textContent = "You lose! Paper beats Rock";
             computerScore = computerScore +1;
+            createPaperMonster ();
+            playerLoseHealth();
             refreshScore();
             return
     }
@@ -100,16 +184,21 @@ function PlayRound(PlayerChoice, ComputerChoice) {
         if (ComputerChoice == "rock") {
             messageField.textContent = "You win! Paper beats Rock";
             playerScore = playerScore + 1;
+            createRockMonster ();
+            monsterLoseHealth();
             refreshScore();
             return
         } 
         if (ComputerChoice == "paper") {
             messageField.textContent = "It's a tie! You both chose Paper";
             refreshScore();
+            createPaperMonster ();
             return
         }
         else messageField.textContent = "You lose! Scissor beats Paper";
             computerScore = computerScore +1;
+            createScissorMonster ();
+            playerLoseHealth();
             refreshScore();
             return
     }
@@ -117,16 +206,21 @@ function PlayRound(PlayerChoice, ComputerChoice) {
         if (ComputerChoice == "paper") {
             messageField.textContent = "You won! Scissor beats Paper";
             playerScore = playerScore + 1;
+            createPaperMonster ();
+            monsterLoseHealth();
             refreshScore();
             return
         } 
         if (ComputerChoice == "scissor") {
             messageField.textContent = "It's a tie! You both chose Scissor";
+            createScissorMonster ();
             refreshScore();
             return
         }
         else messageField.textContent = "You lose! Rock beats Scissor";
             computerScore = computerScore +1;
+            createRockMonster ();
+            playerLoseHealth();
             refreshScore();
             return
     }
